62ae5f615a6c3c208f67229f2949ae2cb71afa5b
Miro Kopecky <kopeckymiroslav@gmail.com>
2016-12-20 01:33:22 +0100

    robo4j-rpi module : RaspberryPi/Arduino

---
+++ robo4j-rpi/src/main/java/com/robo4j/rpi/motor/RpiDevice.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016. Miroslav Kopecky
+ * This RpiDevice.java  is part of robo4j.
+ * module: robo4j-rpi
+ *
+ * robo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * robo4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with robo4j .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.robo4j.rpi.motor;
+
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * @author Miro Kopecky (@miragemiko)
+ * @since 19.12.2016
+ */
+public class RpiDevice implements Closeable {
+
+    protected I2CBus bus;
+    protected I2CDevice device;
+
+    public RpiDevice() {
+    }
+
+    @Override
+    public void close() throws IOException {
+        bus.close();
+    }
+}
