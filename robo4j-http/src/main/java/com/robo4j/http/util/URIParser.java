6c1ad6c0aacac7639670a929d784ac31df29d689
Miro Kopecky <kopeckymiroslav@gmail.com>
2017-01-09 23:03:14 +0100

    clean up

--- robo4j-http/src/main/java/com/robo4j/http/util/URIParser.java
+++ robo4j-http/src/main/java/com/robo4j/http/util/URIParser.java
@@ -24,7 +24,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
