1601a8cea942c586e98ec4a45f4656735168c444
Miro Kopecky <kopeckymiroslav@gmail.com>
2017-01-13 00:36:34 +0100

    Robo4J : packages update

--- robo4j-math/src/main/java/com/robo4j/math/features/FeatureSet.java
+++ robo4j-math/src/main/java/com/robo4j/math/features/FeatureSet.java
@@ -1,18 +1,20 @@
 /*
- * Copyright (C) 2016, Marcus Hirt
- * 
- * Robo4J is free software: you can redistribute it and/or modify
+ * Copyright (C) 2017. Miroslav Wengner, Marcus Hirt
+ * This FeatureSet.java  is part of robo4j.
+ * module: robo4j-math
+ *
+ * robo4j is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Robo4J is distributed in the hope that it will be useful,
+ * robo4j is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
+ * along with robo4j .  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.robo4j.math.features;
 
---
+++ robo4j-math/src/test/java/com/robo4j/math/features/FeatureExtractionTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017. Miroslav Wengner, Marcus Hirt
+ * This FeatureExtractionTest.java  is part of robo4j.
+ * module: robo4j-math
+ *
+ * robo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * robo4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with robo4j .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.robo4j.math.features;
+
+/**
+ * @author Miro Wengner (@miragemiko)
+ * @since 13.01.2017
+ */
+public class FeatureExtractionTest {
+}
