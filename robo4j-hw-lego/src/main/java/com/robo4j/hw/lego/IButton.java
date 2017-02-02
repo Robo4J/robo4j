0f03796ac34e2cd595c5b024078d5bafad382df1
Miroslav Wengner <miro.wengner@gmail.com>
2017-02-02 23:30:54 +0100

    Robo4j: hw-lego, lego-units code separation, escape button handling

---
+++ robo4j-hw-lego/src/main/java/com/robo4j/hw/lego/IButton.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014, 2017. Miroslav Wengner, Marcus Hirt
+ * This IButton.java  is part of robo4j.
+ * module: robo4j-hw-lego
+ *
+ * robo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * robo4j is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with robo4j .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.robo4j.hw.lego;
+
+/**
+ * Methods expoced to the outside
+ *
+ * @author Marcus Hirt (@hirt)
+ * @author Miro Wengner (@miragemiko)
+ * @since 02.02.2017
+ */
+public interface IButton {
+    void waitForPressAndRelease();
+}
