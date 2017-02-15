90142163291073efcfd656c3b47cbb6a3ca90d3c
Marcus Hirt <marcus@hirt.se>
2017-02-16 00:09:31 +0100

    Adding support for scanning.

--- robo4j-units-rpi/src/main/java/com/robo4j/units/rpi/lidarlite/ScanRequest.java
+++ robo4j-units-rpi/src/main/java/com/robo4j/units/rpi/lidarlite/ScanRequest.java
@@ -16,37 +16,71 @@
  */
 package com.robo4j.units.rpi.lidarlite;
 
+/**
+ * This defines how to perform a scan.
+ * 
+ * @author Marcus Hirt (@hirt)
+ * @author Miroslav Wengner (@miragemiko)
+ */
 public class ScanRequest {
+	public enum ScanAction {
+		STOP, ONCE, CONTINUOUSLY
+	}
+
 	private final boolean continuously;
 	private final float startAngle;
 	private final float range;
 	private final float step;
+	private final ScanAction action;
 
 	/**
 	 * 
-	 * @param continuously should the unit keep scanning until told otherwise?
-	 * @param servoRange how much of the full servo range should be scanned (in radians).
+	 * @param continuously
+	 *            should the unit keep scanning until told otherwise?
+	 * @param servoRange
+	 *            how much of the full servo range should be scanned (in
+	 *            radians).
 	 */
-	public ScanRequest(boolean continuously, float startAngle, float range, float step) {
+	public ScanRequest(ScanAction action, boolean continuously, float startAngle, float range, float step) {
+		this.action = action;
 		this.continuously = continuously;
 		this.startAngle = startAngle;
 		this.range = range;
 		this.step = step;
 	}
 
+	/**
+	 * @return true if you want to keep the scan running until told otherwise.
+	 */
 	public boolean isContinuously() {
 		return continuously;
 	}
 
+	/**
+	 * @return the start angle in degrees for the scan.
+	 */
 	public float getStartAngle() {
 		return startAngle;
 	}
 
+	/**
+	 * @return the range of the scan, in degrees.
+	 */
 	public float getRange() {
 		return range;
 	}
 
+	/**
+	 * @return the step, in degrees, between the scans.
+	 */
 	public float getStep() {
 		return step;
+	}
+
+	/**
+	 * @return the kind of {@link ScanAction} to perform.
+	 */
+	public ScanAction getAction() {
+		return action;
 	}
 }
