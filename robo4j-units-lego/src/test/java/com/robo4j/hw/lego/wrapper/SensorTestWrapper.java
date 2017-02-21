a90c27096fe56076927b0a438c54c9407f6e8cb7
Miroslav Wengner <miro.wengner@gmail.com>
2017-02-21 23:24:11 +0100

    code cleanup

--- robo4j-units-lego/src/test/java/com/robo4j/hw/lego/wrapper/SensorTestWrapper.java
+++ robo4j-units-lego/src/test/java/com/robo4j/hw/lego/wrapper/SensorTestWrapper.java
@@ -1,20 +1,18 @@
 /*
- * Copyright (C) 2014, 2017. Miroslav Wengner, Marcus Hirt
- * This SensorTestWrapper.java  is part of robo4j.
- * module: robo4j-units-lego-test
+ * Copyright (c) 2014, 2017, Marcus Hirt, Miroslav Wengner
  *
- * robo4j is free software: you can redistribute it and/or modify
+ * Robo4J is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * robo4j is distributed in the hope that it will be useful,
+ * Robo4J is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with robo4j .  If not, see <http://www.gnu.org/licenses/>.
+ * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.robo4j.hw.lego.wrapper;
@@ -28,7 +26,6 @@ import com.robo4j.hw.lego.enums.SensorTypeEnum;
  *
  * @author Marcus Hirt (@hirt)
  * @author Miro Wengner (@miragemiko)
- * @since 29.01.2017
  */
 public class SensorTestWrapper implements ILegoSensor {
 
