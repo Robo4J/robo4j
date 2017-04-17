7c14ffcf1eef0900f22f1ca048fdc3c4462acb47
Marcus Hirt <marcus@hirt.se>
2017-04-17 19:51:04 +0200

    Fixing typo

--- scripts/rpi/environment.sh
+++ scripts/rpi/environment.sh
@@ -9,6 +9,6 @@ export PI4J_HOME=/opt/pi4j/lib
 #
 # No changes should be required below
 #
-export ROBO4J_PATH=PI4J_HOME/*:$ROBO4J_HOME/robo4j-math/build/libs/robo4j-math-alpha-$ROBO4J_VERSION.jar:$ROBO4J_HOME/robo4j-core/build/libs/robo4j-core-alpha-$ROBO4J_VERSION.jar:$ROBO4J_HOME/robo4j-hw-rpi/build/libs/robo4j-hw-rpi-alpha-$ROBO4J_VERSION.jar:$ROBO4J_HOME/robo4j-units-rpi/build/libs/robo4j-units-rpi-alpha-$ROBO4J_VERSION.jar 
+export ROBO4J_PATH=$PI4J_HOME/*:$ROBO4J_HOME/robo4j-math/build/libs/robo4j-math-alpha-$ROBO4J_VERSION.jar:$ROBO4J_HOME/robo4j-core/build/libs/robo4j-core-alpha-$ROBO4J_VERSION.jar:$ROBO4J_HOME/robo4j-hw-rpi/build/libs/robo4j-hw-rpi-alpha-$ROBO4J_VERSION.jar:$ROBO4J_HOME/robo4j-units-rpi/build/libs/robo4j-units-rpi-alpha-$ROBO4J_VERSION.jar 
 echo Robo4J v$ROBO4J_VERSION
 echo ROBO4J_PATH=$ROBO4J_PATH
