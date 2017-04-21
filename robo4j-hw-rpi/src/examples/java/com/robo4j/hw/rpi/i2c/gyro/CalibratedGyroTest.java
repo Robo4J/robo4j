6d5b3a05800d1c001ebfa4b0f36e33d9a53cdf96
Marcus Hirt <marcus@hirt.se>
2017-04-22 00:26:48 +0200

    Gyro with built in calibration

---
+++ robo4j-hw-rpi/src/examples/java/com/robo4j/hw/rpi/i2c/gyro/CalibratedGyroTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014, 2017, Marcus Hirt, Miroslav Wengner
+ * 
+ * Robo4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Robo4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.robo4j.hw.rpi.i2c.gyro;
+
+import java.io.IOException;
+
+import com.robo4j.hw.rpi.i2c.gyro.GyroL3GD20Device;
+import com.robo4j.hw.rpi.i2c.gyro.GyroL3GD20Device.Sensitivity;
+
+/**
+ * Simple example which repeatedly reads the gyro. Good for checking that your
+ * gyro is working.
+ * 
+ * @author Marcus Hirt (@hirt)
+ * @author Miroslav Wengner (@miragemiko)
+ */
+public class CalibratedGyroTest {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println("Initializing...");
+		GyroL3GD20Device device = new GyroL3GD20Device(Sensitivity.DPS_245);
+		CalibratedGyro gyro = new CalibratedGyro(device);
+		
+		System.out.println("Keep the device still, and press enter to start calibration.");
+		System.in.read();
+		System.out.println("Calibrating...");
+		gyro.calibrate();
+		System.out.println("Calibration done!");
+		
+		while (true) {
+			System.out.println(gyro.read());
+			Thread.sleep(200);
+		}
+	}
+}
---
+++ robo4j-hw-rpi/src/main/java/com/robo4j/hw/rpi/i2c/gyro/CalibratedGyro.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2014, 2017, Marcus Hirt, Miroslav Wengner
+ * 
+ * Robo4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Robo4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.robo4j.hw.rpi.i2c.gyro;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.robo4j.hw.rpi.i2c.CalibratedFloat3DDevice;
+import com.robo4j.hw.rpi.i2c.ReadableDevice;
+import com.robo4j.math.geometry.Float3D;
+
+public class CalibratedGyro extends CalibratedFloat3DDevice {
+	private static final int NUMBER_OF_CALIBRATION_READINGS = 40;
+	private static final int NUMBER_OF_CALIBRATION_READINGS_TO_DROP = 5;
+	private static final Float3D RANGE_MULTIPLIERS = new Float3D(1, 1, -1);
+	
+
+	public CalibratedGyro(ReadableDevice<Float3D> device) {
+		super(device, new Float3D(), RANGE_MULTIPLIERS.copy());
+	}
+
+	public CalibratedGyro(ReadableDevice<Float3D> device, Float3D offsets, Float3D multipliers) {
+		super(device, offsets, multipliers);
+	}
+
+	/**
+	 * Does calibration.
+	 * 
+	 * @throws IOException
+	 */
+	public void calibrate() throws IOException {
+		setCalibration(new Float3D(), Float3D.createIdentity());
+		float[] xvalues = new float[NUMBER_OF_CALIBRATION_READINGS];
+		float[] yvalues = new float[NUMBER_OF_CALIBRATION_READINGS];
+		float[] zvalues = new float[NUMBER_OF_CALIBRATION_READINGS];
+
+		for (int i = 0; i < NUMBER_OF_CALIBRATION_READINGS; i++) {
+			Float3D tmp = read();
+			xvalues[i] = tmp.x;
+			yvalues[i] = tmp.y;
+			zvalues[i] = tmp.z;
+			sleep(20);
+		}
+		Float3D calibration = new Float3D();
+		calibration.x = calibrate(xvalues, NUMBER_OF_CALIBRATION_READINGS_TO_DROP);
+		calibration.y = calibrate(yvalues, NUMBER_OF_CALIBRATION_READINGS_TO_DROP);
+		calibration.z = calibrate(zvalues, NUMBER_OF_CALIBRATION_READINGS_TO_DROP);
+		System.out.println(RANGE_MULTIPLIERS);
+		setCalibration(calibration, RANGE_MULTIPLIERS);
+	}
+	
+	/**
+	 * Simple calibration function. Drop the n highest and lowest, next take
+	 * the average of what is left.
+	 */
+	private float calibrate(float [] values, int drop) {
+		Arrays.sort(values);
+		double calibrationSum = 0;
+		for (int i = drop; i < values.length - drop; i++) {
+			calibrationSum += values[i];
+		}
+		return (float) -calibrationSum / (values.length - 2 * drop);
+	}
+
+	private void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// Don't care
+		}
+	}
+}
