2165ba646cce996d0258dba6de094d9143a0f026
Marcus Hirt <marcus@hirt.se>
2017-09-04 19:40:00 +0200

    Formatting.

--- robo4j-units-rpi/src/examples/java/com/robo4j/units/rpi/gyro/GyroExample.java
+++ robo4j-units-rpi/src/examples/java/com/robo4j/units/rpi/gyro/GyroExample.java
@@ -59,8 +59,7 @@ public class GyroExample {
 
 		System.out.println("Let the gyro unit be absolutely still, then press enter to calibrate and start!");
 		System.in.read();
-		gyro.sendMessage(new GyroRequest(processor, GyroAction.CONTINUOUS,
-				new Tuple3f(1.0f, 1.0f, 1.0f)));
+		gyro.sendMessage(new GyroRequest(processor, GyroAction.CONTINUOUS, new Tuple3f(1.0f, 1.0f, 1.0f)));
 		System.out.println("Will report angular changes indefinitely.\nPress enter to quit!");
 		System.in.read();
 	}
