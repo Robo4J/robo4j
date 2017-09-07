6a6771727ee3ecfae5f116218740119596b558de
Marcus Hirt <marcus@hirt.se>
2017-09-07 22:07:33 +0200

    Spelling

--- robo4j-core/src/main/java/com/robo4j/core/util/CoreConstants.java
+++ robo4j-core/src/main/java/com/robo4j/core/util/CoreConstants.java
@@ -18,14 +18,13 @@
 package com.robo4j.core.util;
 
 /**
- * commonly used constants
+ * Commonly used constants.
  *
  * @author Marcus Hirt (@hirt)
  * @author Miro Wengner (@miragemiko)
  */
 public class CoreConstants {
-
     public static final String STRING_EMPTY = "";
     public static final String STRING_SPACE = "\u0020";
-    public static final CharSequence EMPTY_CHAR_SEQENCE = "";
+    public static final CharSequence EMPTY_CHAR_SEQUENCE = "";
 }
