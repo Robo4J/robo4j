fed70a4d702c815de451d368f135cff758e3b9f3
Marcus Hirt <marcus@hirt.se>
2017-09-17 18:39:20 +0200

    More necessary core updates

--- robo4j-socket-http/src/test/java/com/robo4j/units/httpunit/test/model/ExampleSystem.java
+++ robo4j-socket-http/src/test/java/com/robo4j/units/httpunit/test/model/ExampleSystem.java
@@ -15,7 +15,7 @@
  * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.robo4j.core.units.httpunit.test.model;
+package com.robo4j.units.httpunit.test.model;
 
 import java.util.List;
 
--- robo4j-socket-http/src/test/java/com/robo4j/units/httpunit/test/util/PropertyMapBuilder.java
+++ robo4j-socket-http/src/test/java/com/robo4j/units/httpunit/test/util/PropertyMapBuilder.java
@@ -15,7 +15,7 @@
  * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.robo4j.core.units.httpunit.test.util;
+package com.robo4j.units.httpunit.test.util;
 
 import java.util.Collections;
 import java.util.HashMap;
