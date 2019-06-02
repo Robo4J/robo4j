3ee64fd49da52f82beda1ce949197c7f13411d42
Miro Wengner <miro@robo4j.io>
2019-06-02 20:41:02 +0200

    bno080 : evn script

--- scripts/rpi/environment.sh
+++ scripts/rpi/environment.sh
@@ -7,7 +7,7 @@ export ROBO4J_HOME=/home/pi/git/robo4j
 export ROBO4J_VERSION=0.5
 #export PI4J_HOME=/opt/pi4j/lib
 # Using the 2.0 SNAPSHOT until released
-export PI4J_HOME=~/.m2/repository/com/pi4j/pi4j-core/2.0-SNAPSHOT
+export PI4J_HOME=~/.m2/repository/com/pi4j/pi4j-core/2.0-SNAPSHOT/
 
 # Pre-defined debug flags
 export REMOTE_DEBUG="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=*:9876,suspend=y"
