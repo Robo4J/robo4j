e544422673de73e2c9c804bd35046a9aae0f4aa0
Miro Wengner <miro@robo4j.io>
2019-06-21 20:58:48 +0200

    issue 22: script update

--- scripts/rpi/environment.sh
+++ scripts/rpi/environment.sh
@@ -27,7 +27,7 @@ function createRobo4jPath(){
 
 export ROBO4J_PATH=$(createRobo4jPath)
 export _JAVA_OPTIONS="--add-opens=java.base/jdk.internal.misc=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED"
-echo Robo4J v$ROBO4J_VERSION
+echo ROBO4J v$ROBO4J_VERSION
 echo ROBO4J_PATH=$ROBO4J_PATH
 echo REMOTE_DEBUG=$REMOTE_DEBUG
 
