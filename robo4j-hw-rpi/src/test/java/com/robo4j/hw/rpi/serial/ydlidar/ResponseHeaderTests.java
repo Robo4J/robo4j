cb26a89639735b95772201ddf5d2e7e50dae10e0
Marcus Hirt <marcus@hirt.se>
2019-08-03 16:54:47 +0200

    Adding a few tests

--- robo4j-hw-rpi/src/test/java/com/robo4j/hw/rpi/serial/ydlidar/ResponseHeaderTests.java
+++ robo4j-hw-rpi/src/test/java/com/robo4j/hw/rpi/serial/ydlidar/ResponseHeaderTests.java
@@ -1,10 +1,27 @@
 package com.robo4j.hw.rpi.serial.ydlidar;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class ResponseHeaderTests {
 	@Test
-	void testHeader() {
-		
+	void testInvalidHeader() {
+		byte[] data = new byte[] { (byte) 0xA1, (byte) 0xA2, (byte) 0xA3, (byte) 0xA4, (byte) 0xA5, (byte) 0x99,
+				(byte) 0x99 };
+		ResponseHeader header = new ResponseHeader(data);
+		assertFalse(header.isValid());
 	}
+
+	@Test
+	void testInvalidLength() {
+		byte[] data = new byte[] { (byte) 0xA1, (byte) 0xA2, (byte) 0xA3, (byte) 0xA4, (byte) 0xA5, (byte) 0x99 };
+		try {
+			new ResponseHeader(data);
+			assertTrue(false, "This header should fail!");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
 }
